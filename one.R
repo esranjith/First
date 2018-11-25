6e7b423cc297772735b1f35775f1fc8211eff322
esranjith <ranjith.emursekar@gmail.com>
2018-11-25 14:32:21 +0530

    test commit

---
+++ one.R
@@ -0,0 +1,37 @@
+women
+sort(women)
+x=c(1,2,3,4,5,6,7,8,9,10)
+sort (x)
+sort (x [-c(1,2)])
+x= seq(1,25)
+sort (x)
+(x=seq(1,5, length.out = 15))
+plot (density(x))
+x = seq(1,100)
+quantile(x,seq.1,.01)
+
+x=c(1,2,3,4,5)
+(x)
+sort(x)
+x = 2:6
+sort(x)
+x = c(8,8,9)
+x=seq(1,100)
+(x)
+
+x[2:4]
+plot (density(x))
+plot (hist(x))
+x= rnorm(10000)
+(x)
+plot(density(x))
+lineab(-2,2)
+plot(hist(x),freq=F)
+
+x = c(1,2,3)
+(x)
+x = seq(1,100)
+(x)
+
+x[3:4]
+x 
