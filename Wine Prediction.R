909c921129c31d50e359abc8a0e12a6ee36b467a
esranjith <ranjith.emursekar@gmail.com>
2019-01-01 20:58:08 +0530

    a

--- Wine Prediction.R
+++ Wine Prediction.R
@@ -2,22 +2,27 @@ library(rpart)
 library(rpart.plot)
 library(dplyr)
 
+#--Train
 WineData = read.csv(file=file.choose())
 (WineData)
 testdata = read.csv(file=file.choose())
-fit <- rpart(WineClass ~ ., data = WineData, method = 'class')
+predictdata = read.csv(file=file.choose())
+fit = rpart(WineClass ~ ., data = WineData, method = 'class')
 fit
 rpart.plot(fit, extra = 106, cex=.8,nn=T)  #plot
 rpart.plot(fit)  #plot
 printcp(fit) #select complexity parameter
-prunetree = prune(fit, cp=.2)
+prunetree = prune(fit, cp=.05)
 rpart.plot(fit, cex=.8,nn=T)
-?rpart.plot
 prunetree
+rpart.plot(prunetree) 
 
-
+#--Test
 ClassPredicted= predict(prunetree, newdata=testdata, type='class')
 ClassPredictedProb = predict(prunetree, newdata=testdata, type='prob')
 cbind(testdata,ClassPredicted,ClassPredictedProb)
-ClassPredicted= predict(prunetree, newdata=WineData, type='class')
-cbind(WineData,ClassPredicted)
+
+#--Predit
+FinalPredicted = predict(prunetree, newdata =predictdata, type = 'class')
+FinalPredictedProb = predict(prunetree, newdata = predictdata, type ='prob')
+cbind(predictdata,FinalPredicted,FinalPredictedProb)
